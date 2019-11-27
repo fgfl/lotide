1ae34ce7f1a612bdc7a142ae49e6a96f28424910
Frederick Lee <frederick.gfl@gmail.com>
2019-11-27 02:28:43 +0000

    add assertArrayEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,45 @@
+/*Nov 26, 2019
+  Frederick Lee
+*/
+
+// checks if two arrays are the same.
+// return: true if same. false otherwise
+const eqArrays = function(actualArray, expectedArray) {
+  let isEqual = true;
+
+  if (actualArray.length !== expectedArray.length) {
+    isEqual = false;
+  } else {
+    // loop won't run for empty array b/c length is 0
+    for (let i = 0; i < actualArray.length; ++i) {
+      if (actualArray[i] !== expectedArray[i]) {
+        isEqual = false;
+        break;
+      }
+    }
+  }
+  return isEqual;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  let assertMsg = '';
+  let actualStr = `[${actual}]`;
+  let expectedStr = `[${expected}]`;
+  let isEqual;
+
+  if (eqArrays(actual, expected)) {
+    assertMsg = `✔️ ✔️ ✔️ Assertion Passed: ${actualStr} === ${expectedStr}`;
+    isEqual = true;
+  } else {
+    assertMsg = `❌️❌️❌️Assertion Failed: ${actualStr} === ${expectedStr}`;
+    isEqual = false;
+  }
+  console.log(assertMsg);
+  return isEqual;
+};
+
+// TEST CASES
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([], []);
+assertArraysEqual(['1', 2, 3], [1, 2, 3]);
