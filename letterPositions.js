22136559cce9c4d767f6de8d65e6caf21afa4ef9
Frederick <frederick.gfl@gmail.com>
2019-11-27 22:52:53 +0000

    Add initial file with test

---
+++ letterPositions.js
@@ -0,0 +1,62 @@
+/*Nov 27, 2019
+  Frederick Lee
+*/
+
+// checks if two arrays are the same.
+// return: true if same. false otherwise
+const eqArrays = function(actualArray, expectedArray) {
+  let isEqual = true;
+
+  if (actualArray.length !== expectedArray.length) {
+    isEqual = false;
+  } else {
+    // loop won't run for empty array b/c length is 0
+    for (let i = 0; i < actualArray.length; ++i) {
+      if (actualArray[i] !== expectedArray[i]) {
+        isEqual = false;
+        break;
+      }
+    }
+  }
+  return isEqual;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let assertMsg = '';
+  let actualStr = `[${actual}]`;
+  let expectedStr = `[${expected}]`;
+  let isEqual;
+
+  if (eqArrays(actual, expected)) {
+    assertMsg = `✔️ ✔️ ✔️ Assertion Passed: ${actualStr} === ${expectedStr}`;
+    isEqual = true;
+  } else {
+    assertMsg = `❌️❌️❌️Assertion Failed: ${actualStr} === ${expectedStr}`;
+    isEqual = false;
+  }
+  console.log(assertMsg);
+  return isEqual;
+};
+
+// sentence: input string
+// return: object with arrays for each unique char. The arrays contains the indices of the char
+const letterPositions = function(sentence) {
+  const results = {};
+
+  return results;
+};
+
+
+const sentence = 'lighthouse in the house';
+const result = letterPositions(sentence);
+
+assertArraysEqual(result['l'], [0]);
+assertArraysEqual(result['i'], [1, 11]);
+assertArraysEqual(result['g'], [2]);
+assertArraysEqual(result['h'], [3, 5, 15, 18]);
+assertArraysEqual(result['t'], [4, 14]);
+assertArraysEqual(result['o'], [6, 19]);
+assertArraysEqual(result['u'], [7, 20]);
+assertArraysEqual(result['s'], [8, 21]);
+assertArraysEqual(result['e'], [9, 16, 22]);
+assertArraysEqual(result['n'], [12]);
