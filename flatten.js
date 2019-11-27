811fbb2b033524b6a2a0cd8d31f0d246a192644e
Frederick Lee <frederick.gfl@gmail.com>
2019-11-27 06:04:30 +0000

    lintted flatten.js

--- flatten.js
+++ flatten.js
@@ -2,7 +2,7 @@
 /*Nov 26, 2019
   Frederick Lee
 
-  reate a function flatten which will take in an array of arrays and return a "flattened" version of the array. 
+  reate a function flatten which will take in an array of arrays and return a "flattened" version of the array.
 */
 
 // checks if two arrays are the same.
@@ -45,7 +45,7 @@ const assertArraysEqual = function(actual, expected) {
 const flatten = function(array) {
   let flatArray = [];
   
-  array.forEach((elm, index) => {
+  array.forEach(elm => {
     if (Array.isArray(elm)) {
       elm.forEach(innerElm => {
         flatArray.push(innerElm);
