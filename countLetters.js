d94b4ff26eb658cb1d1e5d1125b49a2cf0f11dcd
Frederick <frederick.gfl@gmail.com>
2019-11-27 22:31:12 +0000

    Added base template for countLetters
    Needs implmentation for countLetters. Example used is from Compass

---
+++ countLetters.js
@@ -0,0 +1,60 @@
+/*Nov 27, 2019
+  Frederick Lee
+*/
+
+const assertEqual = function(actual, expected) {
+  let assertMsg = '';
+  let actualStr;
+  let expectedStr;
+  let isEqual;
+
+  // Enclosed the string with the appropriate wrappers "" or []
+  const makeEnclosedString = function(string) {
+    let output = '';
+    switch (typeof string) {
+    case 'string':
+      output = `"${string}"`;
+      break;
+    default:
+      if (Array.isArray(string)) {
+        output = `[${string}]`;
+      } else {
+        output = string;
+      }
+    }
+    return output;
+  };
+
+  actualStr = makeEnclosedString(actual);
+  expectedStr = makeEnclosedString(expected);
+
+  if (actual === expected) {
+    assertMsg = `✔️ ✔️ ✔️ Assertion Passed: ${actualStr} === ${expectedStr}`;
+    isEqual = true;
+  } else {
+    assertMsg = `❌️❌️❌️Assertion Failed: ${actualStr} === ${expectedStr}`;
+    isEqual = false;
+  }
+  console.log(assertMsg);
+  return isEqual;
+};
+
+const countLetters = function() {
+
+};
+
+
+// TEST CODE
+const input = 'Lighthouse in the house';
+result = countLetters(input);
+
+assertEqual(result['l'], 1);
+assertEqual(result['i'], 2);
+assertEqual(result['g'], 1);
+assertEqual(result['h'], 4);
+assertEqual(result['t'], 2);
+assertEqual(result['o'], 2);
+assertEqual(result['u'], 2);
+assertEqual(result['s'], 2);
+assertEqual(result['e'], 3);
+assertEqual(result['n'], 1);
