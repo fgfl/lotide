5ed1c23ee264b59dabd0d8c267bc2c969e5822b3
Frederick Lee <frederick.gfl@gmail.com>
2019-11-27 05:06:30 +0000

    added middle implementation

--- middle.js
+++ middle.js
@@ -5,7 +5,7 @@
   element(s) of the given array.
 
   The middle function should return an array with only the middle element(s) of the provided array.
-  This means that the length of the returned elements could vary. 
+  This means that the length of the returned elements could vary.
 
   - For arrays with one or two elements, there is no middle. Return an empty array.
   - For arrays with odd number of elements, an array containing a single middle element should be returned.
@@ -50,7 +50,18 @@ const assertArraysEqual = function(actual, expected) {
 
 
 const middle = function(array) {
+  let middleArray = [];
+  const indexMiddle = Math.floor((array.length - 1) / 2);
 
+  if (array.length <= 2) {
+    return middleArray;
+  }
+
+  middleArray.push(array[indexMiddle]);
+  if (array.length % 2 === 0) {
+    middleArray.push(array[indexMiddle + 1]);
+  }
+  return middleArray;
 };
 
 // TEST CASES
