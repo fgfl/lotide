021cfda77e8c3376ad077b93464e209f27a8adb5
Frederick <frederick.gfl@gmail.com>
2019-11-28 22:52:36 +0000

    Add map.js
    Simple template with no logic in map().

---
+++ map.js
@@ -0,0 +1,21 @@
+/*Nov 28, 2019
+  Frederick Lee
+
+Function Definition
+
+Our map function will take in two arguments:
+
+    An array to map
+    A callback function
+
+The map function will return a new array based on the results of the callback function.
+*/
+
+const map = function() {
+
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const result1 = map(words, word => word[0]);
+
+console.log(result1);
