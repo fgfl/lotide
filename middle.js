9a198d8cc520f9f259b431b5639fdf44c57e15e4
Frederick <frederick.gfl@gmail.com>
2019-12-02 21:48:13 +0000

    refactor middle.js. Moved the test code.

--- middle.js
+++ middle.js
@@ -12,42 +12,7 @@
   - For arrays with an even number of elements, an array containing the two elements in the middle should be returned
 */
 
-// checks if two arrays are the same.
-// return: true if same. false otherwise
-const eqArrays = function(actualArray, expectedArray) {
-  let isEqual = true;
-
-  if (actualArray.length !== expectedArray.length) {
-    isEqual = false;
-  } else {
-    // loop won't run for empty array b/c length is 0
-    for (let i = 0; i < actualArray.length; ++i) {
-      if (actualArray[i] !== expectedArray[i]) {
-        isEqual = false;
-        break;
-      }
-    }
-  }
-  return isEqual;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  let assertMsg = '';
-  let actualStr = `[${actual}]`;
-  let expectedStr = `[${expected}]`;
-  let isEqual;
-
-  if (eqArrays(actual, expected)) {
-    assertMsg = `✔️ ✔️ ✔️ Assertion Passed: ${actualStr} === ${expectedStr}`;
-    isEqual = true;
-  } else {
-    assertMsg = `❌️❌️❌️Assertion Failed: ${actualStr} === ${expectedStr}`;
-    isEqual = false;
-  }
-  console.log(assertMsg);
-  return isEqual;
-};
-
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
   let middleArray = [];
@@ -65,11 +30,4 @@ const middle = function(array) {
   return middleArray;
 };
 
-// TEST CASES
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 'test', 3]), ['test']);
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,15 @@
+/*Dec 2, 2019
+  Frederick Lee
+*/
+
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+// TEST CASES
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([1, 'test', 3]), ['test']);
