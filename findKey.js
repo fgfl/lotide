0f8b3a97eb19b5c80b57df4a9c6999238ed4a05c
Frederick <frederick.gfl@gmail.com>
2019-12-03 03:09:49 +0000

    move test code out of findKey.js

--- findKey.js
+++ findKey.js
@@ -6,43 +6,6 @@
   If no key is found, then it should return undefined.
 */
 
-const assertEqual = function(actual, expected) {
-  let assertMsg = '';
-  let actualStr;
-  let expectedStr;
-  let isEqual;
-
-  // Enclosed the string with the appropriate wrappers "" or []
-  const makeEnclosedString = function(string) {
-    let output = '';
-    switch (typeof string) {
-    case 'string':
-      output = `"${string}"`;
-      break;
-    default:
-      if (Array.isArray(string)) {
-        output = `[${string}]`;
-      } else {
-        output = string;
-      }
-    }
-    return output;
-  };
-
-  actualStr = makeEnclosedString(actual);
-  expectedStr = makeEnclosedString(expected);
-
-  if (actual === expected) {
-    assertMsg = `✔️ ✔️ ✔️ Assertion Passed: ${actualStr} === ${expectedStr}`;
-    isEqual = true;
-  } else {
-    assertMsg = `❌️❌️❌️Assertion Failed: ${actualStr} === ${expectedStr}`;
-    isEqual = false;
-  }
-  console.log(assertMsg);
-  return isEqual;
-};
-
 // takes in an object and a callback.
 // obj: object to search
 // stopSearch: callback function to determine when to stop. Stop if true.
@@ -59,18 +22,4 @@ const findKey = function(obj, stopSearch) {
   return retKey;
 };
 
-// TEST CASES
-const hotels = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
-
-// found case
-assertEqual(findKey(hotels, x => x.stars === 2),  "noma");
-
-// not found case
-assertEqual(findKey(hotels, x => x.stars > 4),  undefined);
+module.exports = findKey;
---
+++ test/findKeyTest.js
@@ -0,0 +1,35 @@
+/*Dec 2, 2019
+  Frederick Lee
+*/
+const chai = require('chai');
+const assert = chai.assert;
+
+const findKey = require('../findKey');
+
+describe("#findKey()", () => {
+  it("should return noma when we search for hotels with 2 stars", () => {
+    const hotels = {
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+    // found case
+    assert.equal(findKey(hotels, x => x.stars === 2),  "noma");
+  })
+  it("should return undefined when we search for hotels with more than 4 stars", () => {
+    // TEST CASES
+    const hotels = {
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+    // not found case
+    assert.equal(findKey(hotels, x => x.stars > 4),  undefined);
+  })
+});
