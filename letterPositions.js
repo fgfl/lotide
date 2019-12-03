ec9a2752564986fb2aeaefd6370ca4d8a18698e1
Frederick <frederick.gfl@gmail.com>
2019-12-03 03:26:04 +0000

    Moved test code out of letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -2,42 +2,6 @@
   Frederick Lee
 */
 
-// checks if two arrays are the same.
-// return: true if same. false otherwise
-const eqArrays = function(actualArray, expectedArray) {
-  let isEqual = true;
-
-  if (actualArray.length !== expectedArray.length) {
-    isEqual = false;
-  } else {
-    // loop won't run for empty array b/c length is 0
-    for (let i = 0; i < actualArray.length; ++i) {
-      if (actualArray[i] !== expectedArray[i]) {
-        isEqual = false;
-        break;
-      }
-    }
-  }
-  return isEqual;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  let assertMsg = '';
-  let actualStr = `[${actual}]`;
-  let expectedStr = `[${expected}]`;
-  let isEqual;
-
-  if (eqArrays(actual, expected)) {
-    assertMsg = `✔️ ✔️ ✔️ Assertion Passed: ${actualStr} === ${expectedStr}`;
-    isEqual = true;
-  } else {
-    assertMsg = `❌️❌️❌️Assertion Failed: ${actualStr} === ${expectedStr}`;
-    isEqual = false;
-  }
-  console.log(assertMsg);
-  return isEqual;
-};
-
 // sentence: input string
 // return: object with arrays for each unique char. The arrays contains the indices of the char
 const letterPositions = function(sentence) {
@@ -59,25 +23,4 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-
-const sentence = 'lighthouse in the house';
-const result = letterPositions(sentence);
-
-assertArraysEqual(result['l'], [0]);
-assertArraysEqual(result['i'], [1, 11]);
-assertArraysEqual(result['g'], [2]);
-assertArraysEqual(result['h'], [3, 5, 15, 18]);
-assertArraysEqual(result['t'], [4, 14]);
-assertArraysEqual(result['o'], [6, 19]);
-assertArraysEqual(result['u'], [7, 20]);
-assertArraysEqual(result['s'], [8, 21]);
-assertArraysEqual(result['e'], [9, 16, 22]);
-assertArraysEqual(result['n'], [12]);
-
-const ownSentence = 'hello';
-const result2 = letterPositions(ownSentence);
-
-assertArraysEqual(result2.h, [0]);
-assertArraysEqual(result2.e, [1]);
-assertArraysEqual(result2.l, [2, 3]);
-assertArraysEqual(result2.o, [4]);
+module.exports = letterPositions;
---
+++ test/letterPositions.js
@@ -0,0 +1,34 @@
+/*Dec 2, 2019
+  Frederick Lee
+*/
+const chai = require('chai');
+const assert = chai.assert;
+
+const letterPositions = require('../letterPositions');
+
+describe("#letterPositions()", () => {
+  it("should return the proper indices for 'lighthouse in the house'", () => {
+    const sentence = 'lighthouse in the house';
+    const result = letterPositions(sentence);
+
+    assert.deepEqual(result['l'], [0]);
+    assert.deepEqual(result['i'], [1, 11]);
+    assert.deepEqual(result['g'], [2]);
+    assert.deepEqual(result['h'], [3, 5, 15, 18]);
+    assert.deepEqual(result['t'], [4, 14]);
+    assert.deepEqual(result['o'], [6, 19]);
+    assert.deepEqual(result['u'], [7, 20]);
+    assert.deepEqual(result['s'], [8, 21]);
+    assert.deepEqual(result['e'], [9, 16, 22]);
+    assert.deepEqual(result['n'], [12]);
+  });
+  it("should return the proper inidices for 'hello'", () => {
+    const ownSentence = 'hello';
+    const result2 = letterPositions(ownSentence);
+
+    assert.deepEqual(result2.h, [0]);
+    assert.deepEqual(result2.e, [1]);
+    assert.deepEqual(result2.l, [2, 3]);
+    assert.deepEqual(result2.o, [4]);
+  });
+});
