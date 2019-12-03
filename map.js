f20a384740ef417e9be7b86445105a9fd6f6d8c7
Frederick <frederick.gfl@gmail.com>
2019-12-03 03:53:10 +0000

    Move the test code out of map.js

--- map.js
+++ map.js
@@ -11,43 +11,6 @@ Our map function will take in two arguments:
 The map function will return a new array based on the results of the callback function.
 */
 
-// checks if two arrays are the same.
-// return: true if same. false otherwise
-const eqArrays = function(actualArray, expectedArray) {
-  let isEqual = true;
-
-  if (actualArray.length !== expectedArray.length) {
-    isEqual = false;
-  } else {
-    // loop won't run for empty array b/c length is 0
-    for (let i = 0; i < actualArray.length; ++i) {
-      if (actualArray[i] !== expectedArray[i]) {
-        isEqual = false;
-        break;
-      }
-    }
-  }
-  return isEqual;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  let assertMsg = '';
-  let actualStr = `[${actual}]`;
-  let expectedStr = `[${expected}]`;
-  let isEqual;
-
-  if (eqArrays(actual, expected)) {
-    assertMsg = `✔️ ✔️ ✔️ Assertion Passed: ${actualStr} === ${expectedStr}`;
-    isEqual = true;
-  } else {
-    assertMsg = `❌️❌️❌️Assertion Failed: ${actualStr} === ${expectedStr}`;
-    isEqual = false;
-  }
-  console.log(assertMsg);
-  return isEqual;
-};
-
-
 // The map function will return a new array based on the results of the callback function.
 // array: aray to operate on
 // callback: callback function
@@ -60,24 +23,8 @@ const map = function(array, callback) {
   return result;
 };
 
-const words = ["ground", "control", "to", "major", "tom", '', 1];
-const result1 = map(words, word => word[0]);
-assertArraysEqual(result1, ['g', 'c', 't', 'm', 't', undefined, undefined]);
+module.exports = map;
 
-const numbers = [1, 2, 4, 5, 7, 10];
-const result2 = map(numbers, n => n * n);
-assertArraysEqual(result2, [1, 4, 16, 25, 49, 100]);
-
-const result3 = map(numbers, function(n) {
-  return n / 2;
-});
-assertArraysEqual(result3, [0.5, 1, 2, 2.5, 3.5, 5]);
-
-const findLength = function(word) {
-  return word.length;
-};
-const result4 = map(words, findLength);
-assertArraysEqual(result4, [6, 7, 2, 5, 3, 0, undefined]);
 
 
 
---
+++ test/mapTest.js
@@ -0,0 +1,39 @@
+/*Dec 2, 2019
+  Frederick Lee
+*/
+
+const chai = require('chai');
+const assert = chai.assert;
+
+const map = require('../map');
+
+describe("#map()", () => {
+  it("should return the first letter of each word", () => {
+    const words = ["ground", "control", "to", "major", "tom", '', 1];
+    const result1 = map(words, word => word[0]);
+    assert.deepEqual(result1, ['g', 'c', 't', 'm', 't', undefined, undefined]);
+  });
+  it("should return the square of each number", () => {
+    const numbers = [1, 2, 4, 5, 7, 10];
+    const result2 = map(numbers, n => n * n);
+    assert.deepEqual(result2, [1, 4, 16, 25, 49, 100]);
+  });
+  it("should return half the input numbers if we pass the function in by variable", () => {
+    const numbers = [1, 2, 4, 5, 7, 10];
+    const result3 = map(numbers, function(n) {
+      return n / 2;
+    });
+    assert.deepEqual(result3, [0.5, 1, 2, 2.5, 3.5, 5]);
+  });
+  it("should return the length of the words if we pass the function in by variable", () => {
+    const words = ["ground", "control", "to", "major", "tom", '', 1];
+    const findLength = function(word) {
+      return word.length;
+    };
+    const result4 = map(words, findLength);
+    assert.deepEqual(result4, [6, 7, 2, 5, 3, 0, undefined]);
+  });
+});
+
+
+
