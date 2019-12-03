e79f94f0df29a7674f034c5a6fb04cba06528a8e
Frederick <frederick.gfl@gmail.com>
2019-12-03 00:56:59 +0000

    add index.js to load all modules at the same time

---
+++ index.js
@@ -0,0 +1,36 @@
+/*Dec 2, 2019
+  Frederick Lee
+*/
+
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const {eqObjects, isNonNullObject} = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  isNonNullObject: isNonNullObject,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without,
+};
