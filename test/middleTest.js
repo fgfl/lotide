3f02f51c1c46e67468bebc56113edf3a383c48cb
Frederick <frederick.gfl@gmail.com>
2019-12-03 00:29:02 +0000

    changed middleTest.js to use M&C

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,13 +2,32 @@
   Frederick Lee
 */
 
+const chai = require('chai');
+const assert = chai.assert;
+
 const middle = require('../middle');
 
 // TEST CASES
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 'test', 3]), ['test']);
+describe('#middle()', () => {
+  it('should return [] when array length is 1', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it('should return [] when array length is 2', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it('should return [2] when passed [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it('should return [3] when passed [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it('should return middle two [2, 3] for even length array [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it('should return middle two [3, 4] for even length array [1, 2, 3, 4, 5, 6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it('should return ["test"] from [1, "test", 3]', () => {
+    assert.deepEqual(middle([1, 'test', 3]), ['test']);
+  });
+});
